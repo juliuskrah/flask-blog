503960d078a8bb78ee98a88b868293419b372592
Julius Krah <juliuskrah@gmail.com>
2016-08-04 23:07:23 +0000

    Added 'flash-wtf'

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,8 @@ from flask import Flask
 
 
 app = Flask(__name__)
+app.config.from_object('config')
+
+
 from app import views  # avoids circular import errors when put at the end 'cos the `views` also import `app`
 
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from .forms import LoginForm
 
 
 @app.route('/')
@@ -12,11 +13,26 @@ def index():
             'post': 'This movie is awesome'
         },
         {
-            'author': {'nickname': 'Mary'},
-            'post': 'The match was a draw'
+            'author': {'nickname': 'Ekuba'},
+            'post': 'The best girl'
         }
     ]
     return render_template('index.html',
                            title='Home',
                            user=user,
                            posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
