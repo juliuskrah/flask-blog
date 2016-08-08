485e4583a22db8ab13678703513f6cf67b5762d7
Julius Krah <juliuskrah@gmail.com>
2016-08-08 22:35:48 +0000

    Changed db from 'sqlite' to 'postgres'

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/app'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
